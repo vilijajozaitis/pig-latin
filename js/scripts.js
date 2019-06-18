fc8d1a0a12c50b35d31b3ea32adea329e81e5bc0
hailey Gaylor and Vilija Jozaitis <hailey@>
2019-06-17 19:04:18 -0700

    Arrays and looping concept

---
+++ js/scripts.js
@@ -0,0 +1,16 @@
+$(document).ready(function() {
+  $("form#leap-year").submit(function(event) {
+    event.preventDefault();
+    var year = parseInt($("input#year").val());
+    var result = leapYear(year);
+    $("#result").text(result);
+  });
+  
+    var leapYear = function(year) {
+      if ((year % 4 === 0) && (year % 100 !== 0) || (year % 400 === 0)) {
+        return true;
+      } else {
+        return false;
+      }
+    };
+});
